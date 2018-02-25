37ca755a94ed4fe4feddf72873e9b577e7a86916
lanh97 <lam.thinh1997@yahoo.co>
2018-02-25 08:46:05 +0200

    html:t on nyt kansiossa

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -14,24 +14,38 @@ public class Main {
         if (System.getenv("PORT") != null) {
             Spark.port(Integer.valueOf(System.getenv("PORT")));
         }
-        
-        Database database = new Database("jdbc:sqlite:drinkkiarkisto.db");
+
+        Database database = new Database("jdbc:sqlite:db/drinkkiarkisto.db");
         database.init();
 
         DrinkkiDao drinkkiDao = new DrinkkiDao(database);
 
-        get("/opiskelijat", (req, res) -> {
+        get("/alku", (req, res) -> {
             HashMap map = new HashMap<>();
-            map.put("opiskelijat", drinkkiDao.findAll());
+            map.put("alku", drinkkiDao.findAll());
 
-            return new ModelAndView(map, "opiskelijat");
+            return new ModelAndView(map, "alku");
+        }, new ThymeleafTemplateEngine());
+
+        get("/raaka-aineet/", (req, res) -> {
+            HashMap map = new HashMap<>();
+            map.put("raaka-aineet", drinkkiDao.findAll());
+
+            return new ModelAndView(map, "raaka-aineet");
+        }, new ThymeleafTemplateEngine());
+
+        get("/drinkinraaka-aineet/", (req, res) -> {
+            HashMap map = new HashMap<>();
+            map.put("drinkinraaka-aineet", drinkkiDao.findAll());
+
+            return new ModelAndView(map, "drinkinraaka-aineet");
         }, new ThymeleafTemplateEngine());
 
         get("/drinkit/:id", (req, res) -> {
             HashMap map = new HashMap<>();
-            map.put("drinkki", drinkkiDao.findOne(Integer.parseInt(req.params("id"))));
+            map.put("drinkit", drinkkiDao.findOne(Integer.parseInt(req.params("id"))));
 
-            return new ModelAndView(map, "drinkki");
+            return new ModelAndView(map, "drinkit");
         }, new ThymeleafTemplateEngine());
     }
 }
